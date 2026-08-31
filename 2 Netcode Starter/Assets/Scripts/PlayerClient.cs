0e124d6639c9dbe71c46f67962ee0816de36312f
Jonathan Ngo <143045531+StealthCSUMBStudent@users.noreply.github.com>
2026-08-30 17:38:00 -0700

    Modifications to the best i can

--- 2 Netcode Starter/Assets/Scripts/PlayerClient.cs
+++ 2 Netcode Starter/Assets/Scripts/PlayerClient.cs
@@ -24,8 +24,29 @@ public class PlayerClient : NetworkBehaviour
         {
             _playerInput.enabled = true;
             _starterAssetsInputs.enabled = true;
+            //_thirdPersonController.enabled = true;
+        }
+        if (IsServer)
+        {
             _thirdPersonController.enabled = true;
         }
+    }
+    [Rpc(SendTo.Server)]
+    private void UpdatedInputServerRpc(Vector2 move, Vector2 look, bool jump, bool sprint)
+    {
+        _starterAssetsInputs.MoveInput(move);
+        _starterAssetsInputs.LookInput(look);
+        _starterAssetsInputs.JumpInput(jump);
+        _starterAssetsInputs.SprintInput(sprint);
+    }
+    private void LateUpdate()
+    {
+        if (!IsOwner)
+        {
+            return;
+        }
+
+        UpdatedInputServerRpc(_starterAssetsInputs.move,_starterAssetsInputs.look, _starterAssetsInputs.jump, _starterAssetsInputs.sprint);
     }
     /*
     // Update is called once per frame
